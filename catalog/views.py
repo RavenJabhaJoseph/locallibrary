073ccdbbd237fde79b197d18084d51ccdfe5f118
RavenJabhaJoseph <ravenjabhajoseph@gmail.com>
2021-01-08 11:47:00 +0530

    Created book_detail page and author_detail page. Added pagination style in the pages. Way to go my boy. #self-motivation#self-taught programmer

--- catalog/views.py
+++ catalog/views.py
@@ -22,6 +22,14 @@ def index(request):
 
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 2
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 2
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
