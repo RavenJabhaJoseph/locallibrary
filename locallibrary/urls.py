de75becd1c2f92460ff4f59d654ccc57d7ae3105
RavenJabhaJoseph <ravenjabhajoseph@gmail.com>
2021-01-12 16:18:15 +0530

    Authentication process started

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -35,3 +35,7 @@ urlpatterns += [
 
 #Use static() to add url mapping to serve static files during development
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
